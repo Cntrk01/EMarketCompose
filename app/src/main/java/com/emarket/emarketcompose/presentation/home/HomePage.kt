b54d103b7774eb7f13155fe6bfc3eae99a48c871
Mustafa Cantürk Karabulut <karabulut_3828@hotmail.com>
2024-06-19 23:28:56 +0300

    BottomLoading
    Page sonuna gelince tekrardan data yüklenirken alt tarafta loading durumunun çıkmaısnı istedim bunu ekledim

--- app/src/main/java/com/emarket/emarketcompose/presentation/home/HomePage.kt
+++ app/src/main/java/com/emarket/emarketcompose/presentation/home/HomePage.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.GridItemSpan
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
@@ -20,6 +21,8 @@ import com.emarket.emarketcompose.components.home_card.EMarketHomeCard
 import com.emarket.emarketcompose.components.loading_status.EMarketLoading
 import com.emarket.emarketcompose.components.search.EMarketSearch
 import com.emarket.emarketcompose.components.text.EMarketText
+import com.emarket.emarketcompose.domain.repository.model.EMarketItem
+import com.emarket.emarketcompose.utils.getScreenWidthInDp
 
 @Composable
 fun HomePage(
@@ -27,6 +30,8 @@ fun HomePage(
 ) {
     val homeState = viewModel.homeDataState.collectAsState(HomeState())
     var searchText = remember { "" }
+    var firstLoadings = remember { true }
+    var bottomLoading = remember { true }
 
     Column {
         EMarketHeader(headerTitle = "E-Market App")
@@ -41,12 +46,15 @@ fun HomePage(
         ) {
             homeState.value.apply {
                 if (homeLoading) {
-                    Box(
-                        modifier = Modifier
-                            .fillMaxSize(),
-                        contentAlignment = Alignment.Center
-                    ) {
-                        EMarketLoading()
+                    if (firstLoadings) {
+                        Box(
+                            modifier = Modifier
+                                .fillMaxSize(),
+                            contentAlignment = Alignment.Center
+                        ) {
+                            EMarketLoading()
+                        }
+                        firstLoadings = false
                     }
                 }
                 if (homeError.isNotEmpty()) {
@@ -54,6 +62,7 @@ fun HomePage(
 
                 }
                 if (homeDataList?.isNotEmpty() == true) {
+                    bottomLoading = false
                     EMarketSearch(
                         onValueChange = {
                             searchText = it
@@ -65,37 +74,68 @@ fun HomePage(
 
                     Spacer(modifier = Modifier.padding(top = dimensionResource(id = R.dimen._10dp)))
 
-                    //LazyVerticalStaggeredGrid de kullanabilirim.Fakat ben düzenli gözükmesini istiyorum ondan bunu kullandım.
-                    LazyVerticalGrid(
-                        columns = GridCells.Fixed(2),
-                        content = {
-                            items(homeDataList.size) { index ->
-                                //Apiden homeDataListSize toplam api sonucunun int değerini dönüyor
-                                //homeDataList.size da elimde var olan datayı dönüyor.En son datayı çekince bu değerler birbirine eşit olacağı için if bloğu çalışmıyor böylelikle apiye artık istek atılmıyor
-                                if (index == homeDataList.size-1 && homeDataListSize != homeDataList.size){
-                                    viewModel.loadMoreDataList()
-                                }
-
-                                EMarketHomeCard(
-                                    modifier = Modifier
-                                        .padding(dimensionResource(id = R.dimen._5dp)),
-                                    image = homeDataList[index].image,
-                                    price = homeDataList[index].price,
-                                    description = homeDataList[index].name,
-                                    clickButton = {
-                                        // Card üzerindeki butona tıklama işlemleri
-                                    },
-                                    clickFavorite = {
-                                        // Favoriye ekleme/çıkarma işlemleri
-                                        // Veritabanına yazma işlemi yapılacak
-                                        false
-                                    },
-                                )
-                            }
-                        }
+                    //LazyVerticalStaggeredGrid de kullanabilirim.
+                    //Fakat ben düzenli gözükmesini istiyorum ondan bunu kullandım.
+                    HomeItemLayout(
+                        homeDataList = homeDataList,
+                        homeDataListSize = homeDataListSize,
+                        viewModel = viewModel,
+                        isBottomLoad = bottomLoading
                     )
                 }
             }
         }
     }
 }
+
+@Composable
+fun HomeItemLayout(
+    homeDataList : List<EMarketItem>,
+    homeDataListSize : Int,
+    viewModel: HomeViewModel,
+    isBottomLoad : Boolean,
+){
+    var bottomLoading = remember { isBottomLoad }
+
+    LazyVerticalGrid(
+        columns = GridCells.Adaptive(getScreenWidthInDp() / 3),
+        content = {
+            items(homeDataList.size) { index ->
+                //Apiden homeDataListSize toplam api sonucunun int değerini dönüyor
+                //homeDataList.size da elimde var olan datayı dönüyor.En son datayı çekince bu değerler birbirine eşit olacağı için if bloğu çalışmıyor böylelikle apiye artık istek atılmıyor
+                if (index == homeDataList.size - 1 && homeDataListSize != homeDataList.size) {
+                    viewModel.loadMoreDataList()
+                    bottomLoading=true
+                }
+
+                EMarketHomeCard(
+                    modifier = Modifier
+                        .padding(dimensionResource(id = R.dimen._5dp)),
+                    image = homeDataList[index].image,
+                    price = homeDataList[index].price,
+                    description = homeDataList[index].name,
+                    clickButton = {
+                        // Card üzerindeki butona tıklama işlemleri
+                    },
+                    clickFavorite = {
+                        // Favoriye ekleme/çıkarma işlemleri
+                        // Veritabanına yazma işlemi yapılacak
+                        false
+                    },
+                )
+            }
+
+            item(span = { GridItemSpan(maxLineSpan) }) {
+                //Bottom Loading kullanılmıyor olarak gözüküyor fakat her seferinde bu blok çalışıyor
+                //Bundan dolayı bottomLoading değeri mutlaka olmalı.Örneğin false gelirse Loading durumu çalışmıyor.
+                if (bottomLoading) {
+                    EMarketLoading(
+                        modifier = Modifier
+                            .padding(bottom = dimensionResource(id = R.dimen._10dp))
+                    )
+                }
+            }
+        }
+    )
+}
+
--- app/src/main/java/com/emarket/emarketcompose/utils/Extensions.kt
+++ app/src/main/java/com/emarket/emarketcompose/utils/Extensions.kt
@@ -1,13 +1,23 @@
 package com.emarket.emarketcompose.utils
 
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun dimensionResourceSp(id: Int): TextUnit {
     val density = LocalDensity.current
     val dimension = dimensionResource(id = id)
     return with(density) { dimension.toSp() }
+}
+
+@Composable
+fun getScreenWidthInDp(): Dp {
+    val configuration = LocalConfiguration.current
+    val screenWidthDp = configuration.screenWidthDp.dp
+    return screenWidthDp
 }
