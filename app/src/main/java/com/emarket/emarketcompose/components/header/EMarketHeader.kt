6bb02c6793c3260ccec716ac9ca9179ea34c519c
Mustafa Cantürk Karabulut <karabulut_3828@hotmail.com>
2024-07-02 22:02:04 +0300

    Filter Page

--- app/src/main/java/com/emarket/emarketcompose/components/header/EMarketHeader.kt
+++ app/src/main/java/com/emarket/emarketcompose/components/header/EMarketHeader.kt
@@ -4,15 +4,19 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
@@ -27,13 +31,13 @@ fun EMarketHeader(
     modifier: Modifier = Modifier,
     headerTitle: String = "E-Market",
     headerPadding: Dp = dimensionResource(id = R.dimen._10dp),
+    headerIcon : Int =R.drawable.baseline_arrow_back_24,
     headerType: HeaderType = HeaderType.SIMPLE,
     backClick: () -> Unit = {}
 ) {
     Row(
         modifier = modifier
             .fillMaxWidth()
-            .background(color = colorResource(id = R.color.primaryColor))
             .padding(headerPadding),
         horizontalArrangement = Arrangement.Start,
         verticalAlignment = Alignment.CenterVertically
@@ -44,10 +48,11 @@ fun EMarketHeader(
                     .customClickable {
                         backClick()
                     }
-                    .padding(start = dimensionResource(id = R.dimen._5dp)),
-                imageVector = Icons.Default.ArrowBack,
+                    .padding(start = dimensionResource(id = R.dimen._5dp))
+                    .size(dimensionResource(id = R.dimen._45dp)),
+                painter = painterResource(id = headerIcon),
                 contentDescription = "Back",
-                tint = colorResource(id = R.color.headerTitleIconColor)
+                tint = colorResource(id = R.color.cardTitleColor)
             )
         }
 
@@ -55,7 +60,7 @@ fun EMarketHeader(
             modifier = Modifier.padding(start = dimensionResource(id = R.dimen._15dp)),
             text = headerTitle,
             fontSize = dimensionResourceSp(id = R.dimen._20sp),
-            textColor = colorResource(id = R.color.headerTitleIconColor),
+            textColor = colorResource(id = R.color.cardTitleColor),
             fontWeight = FontWeight.Bold
         )
     }
--- app/src/main/java/com/emarket/emarketcompose/presentation/filter/FilterPage.kt
+++ app/src/main/java/com/emarket/emarketcompose/presentation/filter/FilterPage.kt
@@ -1,9 +1,49 @@
 package com.emarket.emarketcompose.presentation.filter
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.emarket.emarketcompose.R
+import com.emarket.emarketcompose.components.header.EMarketHeader
+import com.emarket.emarketcompose.components.header.HeaderType
+import com.emarket.emarketcompose.ui.theme.EMarketComposeTheme
 
 @Composable
-fun FilterPage(modifier: Modifier = Modifier) {
-    
+fun FilterPage(
+    modifier: Modifier = Modifier,
+    backClick: () -> Unit,
+) {
+    Column(
+        modifier = modifier
+    ){
+        EMarketHeader(
+            modifier = Modifier
+                .background(color = colorResource(id = R.color.background))
+            ,
+            headerPadding= dimensionResource(id = R.dimen._15dp),
+            headerType=HeaderType.MULTI,
+            headerTitle = "Filter",
+            headerIcon = R.drawable.baseline_close_24,
+            backClick = {
+                backClick()
+            }
+        )
+    }
+}
+
+@Preview
+@Composable
+fun PreviewFilterPage(){
+    EMarketComposeTheme {
+        FilterPage(
+            backClick = {
+
+            }
+        )
+    }
 }
